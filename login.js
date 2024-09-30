fe6089cff807e101fda9806ddf568ad21a7002f2
uanderdev <uanderdev@gmail.com>
2024-09-30 17:33:42 -0300

    Nova home finalizada

--- login.js
+++ login.js
@@ -2,3 +2,11 @@
 //Trabalhando no login
 
 //Finalizando sistema de login
+
+
+function login(nome){
+    return nome
+}
+
+// Sistema de login completo
+// Retornando o nome do usuario quando logado
